4ff917e9cbe1941ca7373b8a8048b77ce2c7a01f
codedcoder <busola@bento.africa>
2021-12-07 17:50:48 +0100

    update stack

--- src/components/base/Base.jsx
+++ src/components/base/Base.jsx
@@ -1,50 +1,50 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./Base.css";
-import avi from "../../assets/svgs/codedcoder.svg";
-import navLine from "../../assets/svgs/menu-line.svg";
-import logo from "../../assets/svgs/coded.svg";
-import arrow from "../../assets/svgs/arrow.svg";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Base.css';
+import avi from '../../assets/svgs/codedcoder.svg';
+import navLine from '../../assets/svgs/menu-line.svg';
+import logo from '../../assets/svgs/coded.svg';
+import arrow from '../../assets/svgs/arrow.svg';
 
 export class Base extends Component {
   render() {
     return (
-      <div className="base_container">
-        <div className="hero">
-          <div className="intro_box">
+      <div className='base_container'>
+        <div className='hero'>
+          <div className='intro_box'>
             <h1>
-              {" "}
-              <span className="desktop_grt">Hi,</span>{" "}
-              <span className="mobile_grt">Hi,</span> <br /> I'm Busola.
+              {' '}
+              <span className='desktop_grt'>Hi,</span>{' '}
+              <span className='mobile_grt'>Hi,</span> <br /> I'm Busola.
             </h1>
-            <div className="avi">
-              <img src={avi} alt="avatar" />
+            <div className='avi'>
+              <img src={avi} alt='avatar' />
             </div>
           </div>
 
-          <div className="description">
+          <div className='description'>
             <p>
-              {" "}
+              {' '}
               <span>
-                I am a Full Stack Web Developer with a passion for building
-                things and using code to solve every day problems.{" "}
-              </span>{" "}
-              <span className="highlight">
+                I am a Backend Web Developer with a passion for building things
+                and using code to solve every day problems.{' '}
+              </span>{' '}
+              <span className='highlight'>
                 I participate actively in mentoring young and upcoming
-                developers in Africa as I believe the future is here in Africa.{" "}
-              </span>{" "}
+                developers in Africa as I believe the future is here in Africa.{' '}
+              </span>{' '}
               <span>
-                {" "}
+                {' '}
                 I am always hungry for new knowledge as I know I have only
                 scratched the surface and i'm really looking forward to where
-                this journey leads me.{" "}
+                this journey leads me.{' '}
               </span>
             </p>
           </div>
 
-          <div className="go_button_container">
-            <Link to="/resume">
-              {" "}
+          <div className='go_button_container'>
+            <Link to='/resume'>
+              {' '}
               <button>Explore ➜</button>
             </Link>
           </div>
@@ -54,53 +54,51 @@ export class Base extends Component {
           <ul>
             <li className={this.props.menu[1]}>
               <small>
-                01{" "}
-                <Link to="/resume">
-                  <img src={navLine} alt="menu bullet" /> RESUME
+                01{' '}
+                <Link to='/resume'>
+                  <img src={navLine} alt='menu bullet' /> RESUME
                 </Link>
               </small>
             </li>
             <li className={this.props.menu[0]}>
               <small>
-                02{" "}
-                <Link to="/work">
-                  <img src={navLine} alt="menu bullet" /> WORK
+                02{' '}
+                <Link to='/work'>
+                  <img src={navLine} alt='menu bullet' /> WORK
                 </Link>
               </small>
             </li>
 
             <li className={this.props.menu[2]}>
               <small>
-                03{" "}
-                <Link to="/contact">
-                  <img src={navLine} alt="menu bullet" /> CONTACT
+                03{' '}
+                <Link to='/contact'>
+                  <img src={navLine} alt='menu bullet' /> CONTACT
                 </Link>
               </small>
             </li>
           </ul>
         </nav>
 
-        <div className="footer">
-          <a href="https://codedcoder.com">
-            <img src={logo} alt="codedcoder logo" />
+        <div className='footer'>
+          <a href='https://codedcoder.com'>
+            <img src={logo} alt='codedcoder logo' />
           </a>
 
-          <div className="social">
+          <div className='social'>
             <a
-              href="https://github.com/codedcoderr"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-github"></i> Github{" "}
-              <img src={arrow} alt="pointer" />
+              href='https://github.com/codedcoderr'
+              target='_blank'
+              rel='noopener noreferrer'>
+              <i className='fab fa-github'></i> Github{' '}
+              <img src={arrow} alt='pointer' />
             </a>
             <a
-              href="https://twitter.com/codedcoder"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-twitter"></i> Twitter{" "}
-              <img src={arrow} alt="pointer" />
+              href='https://twitter.com/codedcoder'
+              target='_blank'
+              rel='noopener noreferrer'>
+              <i className='fab fa-twitter'></i> Twitter{' '}
+              <img src={arrow} alt='pointer' />
             </a>
           </div>
         </div>
